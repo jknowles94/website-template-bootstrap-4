ae759abb7253ba474db0cce8370eb9c2e7a86111
Jake Knowles <jakeyknowles@hotmail.co.uk>
2017-09-08 16:56:52 +0100

    Gulp file added - basic watch task

--- README.md
+++ README.md
@@ -1,17 +1,3 @@
 # website-template-bootstrap-4
 
 Website Template quick set up using Bootstrap 4-beta (https://getbootstrap.com/docs/4.0/migration/)
-
-https://github.com/postcss/gulp-postcss
-
-https://browsersync.io/docs/gulp/
-
-https://github.com/contra/gulp-concat
-
-https://github.com/terinjokes/gulp-uglify
-
-https://github.com/terinjokes/gulp-uglify/blob/master/docs/why-use-pump/README.md#why-use-pump
-
-https://www.npmjs.com/package/gulp-inject
-
-https://github.com/gulpjs/gulp-util
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,53 @@
 var gulp = require("gulp");
+var postCss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssNano = require("gulp-cssnano");
+var concat = require("gulp-concat");
+var inject = require("gulp-inject");
+var uglify = require("gulp-uglify");
+var sass = require("gulp-sass");
+var wiredep = require('wiredep').stream;
+var notify = require("gulp-notify");
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task("sass", function() {
+  return gulp.src("assets/sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("assets/styles"))
+    .pipe(notify("Sass Updated"))
+});
+
+gulp.task("postCss", function() {
+  var plugins = [
+    autoprefixer({browsers: ["last 3 version"]}),
+    cssNano()
+  ];
+  return gulp.src("assets/css/*.css")
+    .pipe(postCss(plugins));
+});
+
+//On setup run Inject to add your files to the index.html
+
+gulp.task("inject", function() {
+  var target = gulp.src("index.html");
+  var sources = gulp.src(["assets/scripts/*.js", "assets/styles/*.css"], {read: false});
+
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("wiredep", function() {
+  gulp.src('index.html')
+    .pipe(wiredep({}))
+    .pipe(gulp.dest('.'))
+});
+
+gulp.task("watch", function() {
+  gulp.watch("assets/sass/*.scss", ["sass", "postCss"]);
+  gulp.watch("bower.json", ["wiredep"]);
+});
+
+gulp.task("build", function() {
+  //concat all plugins into here
+  //concat all scripts you made into a scripts file
+  //uglify both scripts
 });
